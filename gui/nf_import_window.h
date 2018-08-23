9ac3bc5249e8b10e146b087cbd0f1c25695bd5c6
ben <ben@ELECTRON>
2018-08-23 09:15:23 -0500

    more nf_import_window

--- gui/nf_import_window.h
+++ gui/nf_import_window.h
@@ -15,21 +15,21 @@ class nf_import_window : public QMainWindow {
 	nf_import_window(QWidget *parent = Q_NULLPTR);
 
 	private:
-		//---------------------------------------------------------------------------
-		// Data
-		au::uih_parser<parse_userinput_ts> m_ts_parser {parse_userinput_ts {},
-			"The time signature (format: n/d[c] where c means \"compound\")"};
-		au::uih<decltype(m_ts_parser)> m_ts_uih {m_ts_parser};
+	//----------------------------------------------------------------------------
+	// Data
+	au::uih_parser<parse_userinput_ts> m_ts_parser {parse_userinput_ts {},
+		"The time signature (format: n/d[c] where c means \"compound\")"};
+	au::uih<decltype(m_ts_parser)> m_ts_uih {m_ts_parser};
 
-		au::uih_parser<parse_userinput_double> m_bpm_parser {parse_userinput_double {},
-			"The tempo in beats-per-minute"};
-		au::uih_pred<ftr_gt> p_gtzero {ftr_gt{0.0},"A tempo is always > 0"};
-		au::uih<decltype(m_bpm_parser),decltype(p_gtzero)> m_bpm_uih {m_bpm_parser,p_gtzero};
+	au::uih_parser<parse_userinput_double> m_bpm_parser {parse_userinput_double {},
+		"The tempo in beats-per-minute"};
+	au::uih_pred<ftr_gt> p_gtzero {ftr_gt{0.0},"A tempo is always > 0"};
+	au::uih<decltype(m_bpm_parser),decltype(p_gtzero)> m_bpm_uih {m_bpm_parser,p_gtzero};
 
-		au::uih_parser<parse_userinput_double> m_err_parser {parse_userinput_double {},
-			"A number >= 0 (decimals allowed)."};
-		au::uih_pred<ftr_geq> p_geqzero {ftr_geq{0.0},"Value must be >= 0."};
-		au::uih<decltype(m_err_parser),decltype(p_geqzero)> m_err_uih {m_err_parser,p_geqzero};
+	au::uih_parser<parse_userinput_double> m_err_parser {parse_userinput_double {},
+		"A number >= 0 (decimals allowed)."};
+	au::uih_pred<ftr_geq> p_geqzero {ftr_geq{0.0},"Value must be >= 0."};
+	au::uih<decltype(m_err_parser),decltype(p_geqzero)> m_err_uih {m_err_parser,p_geqzero};
 
 	struct defaults {
 		std::string ts {"4/4"};
@@ -40,14 +40,6 @@ class nf_import_window : public QMainWindow {
 		std::string init_dir {"..\\stuff\\"};
 	};
 	defaults m_defaults {};
-
-	//-------------------------------
-	//struct status_flags {
-	//	bool curr_fname {false};
-	//	bool nf {false};
-	//	bool nf_table {false};
-	//};
-	//status_flags m_status {};
 
 	struct qt_nf_table_data {
 		QTableWidgetItem ontime {0.0};
@@ -82,24 +74,28 @@ class nf_import_window : public QMainWindow {
 	
 	void set_nftable();
 	void set_nftable(int const r);
-		// Populates the gui widget from m_nf_table_data.  Overload 
+		// Populates the gui widget from m_nf/m_nf_table_data.  Overload 
 		// set_nftable(int const r) does only one row of the table,
 		// overload set_nftable() clears the entire table widget then
 		// calls overload set_nftable(int const r) for every row in the
 		// backend datastore m_nf/m_nf_table_data.  
 		// Overload m_nf_table_data(int const r) is responsible for 
 		// setting cell colors for valid/invalid cells.  
+		//
+		// TODO:  This should blank out all rows below 
+		// m_nf_table.data.size() as a part of syncing w/ m_nf_table_data.  
 
 	void update_nv_t_count();
 	void set_ts();
 	void set_bpm();
 	void set_err();
 	
-
+	// TODO:  No longer needed...
 	std::vector<double> qt_table2double(int const, int const, int const,
 		double const) const;
 
 	Ui::nf_import_window ui;
+
 	private slots:
 	void on_ts_returnPressed();
 	void on_ts_textEdited();
@@ -108,8 +104,6 @@ class nf_import_window : public QMainWindow {
 	void on_err_returnPressed();
 	void on_err_textEdited();
 	void on_nf_table_cellChanged(int, int);
-
-	// "Cancel" and "import" buttons
 	void on_cancel_clicked();
 	void on_import_clicked();
 };
