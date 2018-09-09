22f9d495d864e28bc0e89d9999908691a4bcfa61
ben <ben@ELECTRON>
2018-09-09 07:20:52 -0500

    tmetg_t rp2mg

--- aulib/types/metg_t.cpp
+++ aulib/types/metg_t.cpp
@@ -1,10 +1,12 @@
 #include "metg_t.h"
 #include "ts_t.h"
+#include "nv_t.h";
 #include "beat_bar_t.h"
 #include "rp_t.h"
 #include "..\util\au_error.h"
 #include "..\util\au_random.h"
 #include "..\util\au_algs_math.h"
+#include "..\util\au_algs.h"
 #include "..\util\au_util.h"  // wait()
 #include <vector>
 #include <numeric> // lcm, gcd
@@ -18,6 +20,53 @@
 
 // Constructors
 
+tmetg_t::tmetg_t(ts_t ts_in, rp_t rp_in) {
+	m_ts = ts_in;
+	auto vdt = rp_in.to_duration_seq();
+
+	beat_t curr_bt {0};
+	for (const auto& e : vdt) {
+		double nr = std::round(curr_bt/nbeat(m_ts,e));
+		beat_t ph = curr_bt-nr*nbeat(m_ts,e);
+		nvs_ph curr_nvsph {e, nbeat(m_ts,e), ph};
+		if (std::find(m_nvsph.begin(),m_nvsph.end(),curr_nvsph) == m_nvsph.end()) {
+			m_nvsph.push_back(curr_nvsph);
+		}
+		curr_bt += nbeat(m_ts,e);
+	}
+	std::sort(m_nvsph.begin(),m_nvsph.end(),
+		[](const nvs_ph& a, const nvs_ph& b){return a.nv>b.nv;});
+
+	m_btres = gres();
+	m_period = period();
+
+	// The rp passed in may be > or < 1 period.  If < or if 
+	// !aprx_integer(curr_bt/m_period), it is not possible to sample longer
+	// rp's by extending m_pg...  What did i do in the matlab version?
+	// Flag such situations
+	if (!aprx_int(m_period/curr_bt)) {
+		m_f_pg_extends = false;  // Default is true
+	}
+	auto pg_nsteps = static_cast<int>(std::max(m_period,curr_bt)/m_btres);
+	m_pg = std::vector<std::vector<pgcell>>(pg_nsteps,std::vector<pgcell>{});
+
+	curr_bt = 0_bt;
+	int curr_idx = 0;
+	for (const auto& e : vdt) {
+		auto idx = levels_allowed(curr_bt);
+		int stepsz = static_cast<int>(nbeat(m_ts,e)/m_btres);
+		for (auto i=0; i<idx.size(); ++i) {
+			if (m_nvsph[idx[i]].nv == e) {
+				m_pg[curr_idx].push_back({idx[i],stepsz,1});
+			}
+		}
+		curr_bt += nbeat(m_ts,e);
+		curr_idx += nbeat(m_ts,e)/m_btres;
+	}
+
+	wait();
+}
+
 // TODO:  This should take an options type-arg to set if bar-spanning elements 
 // should be allowed... this then changes the gres and period calcs.  
 tmetg_t::tmetg_t(ts_t ts_in, std::vector<d_t> nvs_in, std::vector<beat_t> ph_in) {
@@ -30,31 +79,8 @@ tmetg_t::tmetg_t(ts_t ts_in, std::vector<d_t> nvs_in, std::vector<beat_t> ph_in)
 		m_nvsph.push_back({nvs_in[i], nbeat(ts_in,nvs_in[i]), ph_in[i]});
 	}
 
-	// Minimum grid resolution
-	//  TODO:  Overload of gcd for beat_t
-	d_t gres = gcd(d_t{0.0}, m_ts.bar_unit());
-	for (auto const& e : m_nvsph) {
-		gres = gcd(gres, e.nv);
-		gres = gcd(gres, duration(m_ts,e.ph));
-	}
-	
-	// Minimum grid period
-	// TODO:  Just do the whole thing in beat_t
-	int n_gres_units = m_ts.bar_unit()/gres;
-	int ce = 0;
-	for (auto e : m_nvsph) {
-		ce = e.nv/gres;
-		n_gres_units = std::lcm(n_gres_units, ce);
-
-		ce = e.ph/nbeat(m_ts,gres);
-		if (ce > 0) {
-			// TODO:  should take the abs() of a -ph
-			n_gres_units = std::lcm(n_gres_units, ce);
-		}
-	}
-	auto gperiod = n_gres_units*gres;
-	m_btres = beat_t{gres/m_ts.beat_unit()};
-	m_period = beat_t{gperiod/m_ts.beat_unit()};
+	m_btres = gres();
+	m_period = period();
 
 	// The pg is always initialized to this dummy value to obviate the
 	// need to add a bool is_pg_set check on every method that reads the
@@ -68,6 +94,39 @@ tmetg_t::tmetg_t(ts_t ts_in, std::vector<d_t> nvs_in, std::vector<beat_t> ph_in)
 	}
 
 	set_rand_pg();
+}
+
+
+
+beat_t tmetg_t::gres() const {
+	// Minimum grid resolution
+	//  TODO:  Overload of gcd for beat_t ?
+	d_t gres = gcd(d_t{0.0}, m_ts.bar_unit());
+	for (auto const& e : m_nvsph) {
+		gres = gcd(gres, e.nv);
+		gres = gcd(gres, duration(m_ts,e.ph));
+	}
+	return nbeat(m_ts,gres);
+}
+
+beat_t tmetg_t::period() const {  // Depends on m_btres being correctly set
+	// Minimum grid period
+	// TODO:  Just do the whole thing in beat_t ?
+	auto nvgres = duration(m_ts,m_btres);
+	int n_gres_units = m_ts.bar_unit()/nvgres;
+	int ce = 0;
+	for (auto e : m_nvsph) {
+		ce = e.nv/nvgres;
+		n_gres_units = std::lcm(n_gres_units, ce);
+
+		ce = e.ph/nbeat(m_ts,nvgres);
+		if (ce > 0) {
+			// TODO:  should take the abs() of a -ph
+			n_gres_units = std::lcm(n_gres_units, ce);
+		}
+	}
+	auto gperiod = n_gres_units*nvgres;
+	return beat_t{gperiod/m_ts.beat_unit()};
 }
 
 void tmetg_t::set_rand_pg() {
@@ -133,32 +192,27 @@ std::vector<double> tmetg_t::nt_prob(beat_t bt) const {
 void tmetg_t::enumerate() const {
 	int ncols = static_cast<int>(m_period/m_btres);
 
-	// g is essentially a copy of m_pg, however, instead of each col having
-	// m_nvsph rows, each col has between 0 and m_nvsph rows.  For each col, 
-	// entries appear in order of _decreasing_ probability.  Entries in m_pg 
-	// with p == 0.0 do not appear in g.  	
-	int N_tot_guess =1;
-	std::vector<std::vector<pgcell>> g(ncols,std::vector<pgcell>{});
-	for (int i=0; i<ncols; ++i) {
-
-		std::vector<double> curr_col_probs;// = m_pg2[i];
-		for (auto const& e : m_pg[i]) {
-			curr_col_probs.push_back(e.lgp);
+	// g is derrived from m_pg:  
+	// 1)  Each col of g contains only the entries in the corresponding col of
+	// m_pg where the probability is > 0.  Hence, each col of g in principle
+	// contains a different number of rows.  
+	// 2)  Entries appear in each col of g appear in order of _decreasing_ 
+	// probability, not in order of idx in m_nvsph.  
+	// 3)  g[i][j].lgp is the std::log() of the probability of the corresponding
+	// element in m_pg.  
+	int N_tot_guess = 1;
+	std::vector<std::vector<pgcell>> g {};
+	for (auto e : m_pg) {
+		std::sort(e.begin(),e.end(),
+			[](const pgcell& a, const pgcell& b){return a.lgp>b.lgp;});
+		std::vector<pgcell> e_nz {};
+		for (auto ee : e) {
+			if (ee.lgp == 0.0) { break;}
+			e_nz.push_back(ee);
+			e_nz.back().lgp = std::log(e_nz.back().lgp);
 		}
-		while (std::any_of(curr_col_probs.begin(),curr_col_probs.end(),[](double d){return d>0.0;})) {
-			auto it = std::max_element(curr_col_probs.begin(),curr_col_probs.end());
-			auto idx = std::distance(curr_col_probs.begin(),it);
-			if (idx < curr_col_probs.size()) {  // Shouldn't need this test
-				g[i].push_back(m_pg[i][idx]);
-				g[i].back().lgp = std::log(g[i].back().lgp);
-				curr_col_probs[idx] = 0.0;
-			}
-		}
-
-		// Just a crude guess at the total number of rps possible
-		if (g[i].size() > 0) {
-			N_tot_guess *= g[i].size();
-		}
+		g.push_back(e_nz);
+		N_tot_guess *= std::max(g.back().size(),static_cast<size_t>(1));
 	}
 
 	int N_prealloc = std::min(10000,N_tot_guess);
@@ -170,6 +224,7 @@ void tmetg_t::enumerate() const {
 	m_enumerator(rps,g,N,x);
 
 	// Crude print
+	/*
 	std::vector<double> p {};
 	for (int i=0; i<N; ++i) {
 		p.push_back(rps[i].p);
@@ -181,7 +236,7 @@ void tmetg_t::enumerate() const {
 			s += " ";
 		}
 		std::cout << s << std::endl;
-	}
+	}*/
 
 	wait();
 }
@@ -264,6 +319,17 @@ bool tmetg_t::allowed_at(beat_t beat) const {
 	return false;
 }
 
+// True if nv can occur at the given beat
+bool tmetg_t::allowed_at(d_t nv, beat_t beat) const {
+	for (auto const& e : m_nvsph) {
+		if (e.nv != nv) {continue;}
+		if (aprx_int((beat-e.ph)/e.nbts)) {
+			return true;
+		}
+	}
+	return false;
+}
+
 //  Which levels of the grid are allowed at the given beat?
 std::vector<int> tmetg_t::levels_allowed(beat_t beat) const {
 	std::vector<int> allowed {}; allowed.reserve(m_nvsph.size());
@@ -275,7 +341,6 @@ std::vector<int> tmetg_t::levels_allowed(beat_t beat) const {
 	}
 	return allowed;
 }
-
 
 std::string tmetg_t::print() const {
 	std::string s {};
@@ -314,6 +379,29 @@ std::string tmetg_t::print() const {
 	return s;
 }
 
+std::string tmetg_t::print_pg() const {
+	std::string s {};
+
+	std::vector<std::vector<double>> pg_full {}; //(m_pg.size(),std::vector<double>{});
+	for (const auto& e : m_pg) {
+		std::vector<double> curr_col(m_nvsph.size(),0.0);
+		for (const auto& ee : e) {
+			curr_col[ee.ix_nvsph] = ee.lgp;
+		}
+		pg_full.push_back(curr_col);
+	}
+
+	int ridx {0};
+	for (int ridx=0; ridx<m_nvsph.size(); ++ridx) {
+		for (const auto& c : pg_full) {
+			s += bsprintf("%5.3f   ",c[ridx]);
+		}
+		s += "\n";
+	}
+
+	return s;
+}
+
 
 d_t tmetg_t::gcd(const std::vector<d_t>& dset) const {
 	long long sfctr = 100'000'000'000;
@@ -337,22 +425,37 @@ d_t tmetg_t::gcd(const d_t& a, const d_t& b) const {
 	return d_t {cgcd/sfctr};
 }
 
+// TODO:   Generalize this to any T
+beat_t tmetg_t::round(beat_t bt) const {
+	return beat_t{std::round(bt/(1_bt))};
+}
+
 std::string autest::metg::tests1() {
+	auto s = std::string();
 	std::vector<d_t> dt1 {d::h,d::q,d::ed,d::e};
 	std::vector<beat_t> ph1(dt1.size(),beat_t{0});
 	auto mg = tmetg_t(ts_t{beat_t{3},d::q},dt1,ph1);
 
-	auto s = mg.print();
-	std::cout <<s <<std::endl<<std::endl<<std::endl;
+	//auto s = mg.print();
+	//std::cout <<s <<std::endl<<std::endl<<std::endl;
 
 	mg.set_rand_pg();
-	for (int i=0; i < 5; ++i) {
+	/*for (int i=0; i < 5; ++i) {
 		auto vdt = mg.draw();
 		rp_t rp {ts_t{3_bt,d::q},vdt};
 		std::cout <<rp.print() <<std::endl<<std::endl<<std::endl;
+		wait();
 	}
+	mg.enumerate();*/
 
-	mg.enumerate();
+	auto vdt = mg.draw();
+	rp_t rp {ts_t{3_bt,d::q},vdt};
+	std::cout <<rp.print() <<std::endl<<std::endl<<std::endl;
+
+	tmetg_t mg2 {ts_t{3_bt,d::q},rp};
+	std::cout << mg2.print_pg() <<std::endl<<std::endl<<std::endl;
+	std::cout << mg2.print() <<std::endl<<std::endl<<std::endl;
+	
 
 	wait();
 	return s;
--- aulib/types/metg_t.h
+++ aulib/types/metg_t.h
@@ -2,6 +2,7 @@
 #include "beat_bar_t.h"
 #include "ts_t.h"
 #include "nv_t.h"
+#include "rp_t.h";
 #include <vector>
 #include <string>
 
@@ -11,6 +12,7 @@
 class tmetg_t {
 public:
 	tmetg_t() = delete;
+	tmetg_t(ts_t, rp_t);
 	// ts, dp, phases
 	explicit tmetg_t(ts_t,std::vector<d_t>,std::vector<beat_t>);
 
@@ -23,12 +25,16 @@ public:
 	bool allowed_at(beat_t) const;
 	bool allowed_next(beat_t,d_t) const;
 	std::string print() const;
-	
+	std::string print_pg() const;
+
 private:
 	struct nvs_ph {
 		d_t nv {};
 		beat_t nbts {};
 		beat_t ph {};
+		bool operator==(const nvs_ph& rhs) const {
+			return ((nv == rhs.nv) && (nbts == rhs.nbts) && (ph == rhs.ph));
+		};
 	};
 	std::vector<nvs_ph> m_nvsph {};
 	ts_t m_ts {4_bt,d_t{d::q}};
@@ -41,30 +47,35 @@ private:
 
 	// Probability grid
 	struct pgcell {
-		int ix_nvsph {0};
+		int ix_nvsph {0};  // Idx of m_nvsph
 		int stepsz {0};
 		double lgp {0.0};
 	};
-	//std::vector<std::vector<double>> m_pg {};
 	std::vector<std::vector<pgcell>> m_pg {};
+	bool m_f_pg_extends {true};
 
 	struct rpp {  // "rp with probability"
 		std::vector<int> rp {};
 		double p {1.0};
 	};
 
+	bool allowed_at(d_t, beat_t) const;
 	std::vector<int> levels_allowed(beat_t) const;
 		// idx to allowed nv's @ the given beat; these numerical
 		// indices mean nothing to an external caller, hence this
 		// method is private.  
 
-	//void m_enumerator(std::vector<std::vector<int>>&, 
-	//	std::vector<std::vector<int>> const&, int&,int&) const;
+	//----------------------------------------------------------------------------
+	// Methods
+	beat_t gres() const;
+	beat_t period() const;
+
 	void m_enumerator(std::vector<rpp>&, 
 		std::vector<std::vector<pgcell>> const&, int&, int) const;
 	
 	d_t gcd(const std::vector<d_t>&) const;  // greatest common divisor
 	d_t gcd(const d_t&, const d_t&) const;
+	beat_t round(beat_t) const;
 };
 
 
