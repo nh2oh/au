cd2ac4ddff869aa2d5487c5188892623fda37d3e
nh2oh <ben@ionophore.com>
2018-11-29 12:16:03 -0600

    frq_t initial cleanup

--- aulib/types/frq_t.cpp
+++ aulib/types/frq_t.cpp
@@ -1,15 +1,20 @@
 #include "frq_t.h"
 #include "cent_oct_t.h"
 #include "..\util\au_algs_math.h"
-#include "..\util\au_util.h" // bsprintf()
+//#include "..\util\au_util.h" // bsprintf()
 #include <string>
-#include <cmath>  // pow(), log2()
-#include <cstdlib> // div()... not in <cmath>
+//#include <cmath>  // pow(), log2()
+//#include <cstdlib> // div()... not in <cmath>
+#include <exception>
 
 // Static members
-const int frq_t::m_prec {9};
+//const int frq_t::m_prec {9};
 
-frq_t::frq_t(double const& frq_in, unit const& unit_in) {
+frq_t::frq_t(double d) {
+	if (d < 0.0) { std::abort(); }
+	m_frq = d;
+}
+/*frq_t::frq_t(double frq_in, unit const& unit_in) {
 	switch(unit_in) {
 		case unit::Hz:  m_unit = 0; break;
 		case unit::kHz: m_unit = 3; break;
@@ -17,10 +22,10 @@ frq_t::frq_t(double const& frq_in, unit const& unit_in) {
 		case unit::GHz: m_unit = 9; break;
 	}
 	m_frq = fmax(0.0, frq_in*(std::pow(10,m_unit)));
-}
+}*/
 
-std::string frq_t::print(int prec) const {
-	std::string fmt {"%f"};
+std::string frq_t::print() const {
+	/*std::string fmt {"%f"};
 	if (prec > 0) { 
 		fmt += "." + std::to_string(prec);
 	}
@@ -36,70 +41,67 @@ std::string frq_t::print(int prec) const {
 		default: unitstr = "? unit"; break;
 	}
 
-	return dbk::bsprintf(fmt.c_str(),m_frq,unitstr.c_str());
+	return dbk::bsprintf(fmt.c_str(),m_frq,unitstr.c_str());*/
+	return std::to_string(m_frq);
 }
 
 // Member operators
-// The /, *=, +=, -= operators do not need to deal with m_unit to make
-// the calculation because m_frq is always stored in Hz.  
-// Likewise, == and > can compare m_frq directly and ignore m_unit.  
-double frq_t::operator/(frq_t const& denom) const {
+double frq_t::operator/(const frq_t& denom) const {
 	return m_frq/(denom.m_frq);
 }
-frq_t& frq_t::operator*=(double const& d) {
+frq_t& frq_t::operator*=(double d) {
 	m_frq *= d;
-	m_frq = fmax(m_frq,0.0);
+	if (m_frq < 0.0) { std::abort(); }
 	return *this;
 }
-frq_t& frq_t::operator/=(double const& d) {
+frq_t& frq_t::operator/=(double d) {
 	m_frq /= d;
-	m_frq = fmax(m_frq,0.0);
+	if (m_frq < 0.0) { std::abort(); }
 	return *this;
 }
-frq_t& frq_t::operator+=(frq_t const& rhs) {
+frq_t& frq_t::operator+=(const frq_t& rhs) {
 	m_frq += rhs.m_frq;
 	return *this;
 }
-frq_t& frq_t::operator-=(frq_t const& rhs) {
+frq_t& frq_t::operator-=(const frq_t& rhs) {
 	m_frq -= rhs.m_frq;
-	m_frq = fmax(m_frq,0.0);
+	if (m_frq < 0.0) { std::abort(); }
 	return *this;
 }
-bool frq_t::operator==(frq_t const& rhs) const {
+bool frq_t::operator==(const frq_t& rhs) const {
 	return aprx_eq(m_frq,rhs.m_frq);
 }
-bool frq_t::operator>(frq_t const& rhs) const {
+bool frq_t::operator!=(const frq_t& rhs) const {
+	return !aprx_eq(m_frq,rhs.m_frq);
+}
+bool frq_t::operator<(const frq_t& rhs) const {
+	return aprx_lt(m_frq,rhs.m_frq);
+}
+bool frq_t::operator>(const frq_t& rhs) const {
 	return aprx_gt(m_frq,rhs.m_frq);
 }
-
+bool frq_t::operator<=(const frq_t& rhs) const {
+	return !aprx_gt(m_frq,rhs.m_frq);
+}
+bool frq_t::operator>=(const frq_t& rhs) const {
+	return !aprx_lt(m_frq,rhs.m_frq);
+}
 
 // Nonmember operators
-// For *, + and -, the units of the result are those of the lhs.  Since
-// these return a new object, the lhs is always passed in by value.  
-frq_t operator+(frq_t lhs, frq_t const& rhs) {
+// For *, + and -, the units of the result are those of the lhs.  Since these return a new object,
+// the lhs is always passed in by value.  
+frq_t operator+(frq_t lhs, const frq_t& rhs) {
 	return (lhs += rhs);
 }
-frq_t operator-(frq_t lhs, frq_t const& rhs) {
+frq_t operator-(frq_t lhs, const frq_t& rhs) {
 	return (lhs -= rhs);
 }
-frq_t operator*(double const& lhs, frq_t rhs) {
+frq_t operator*(double lhs, frq_t rhs) {
 	return (rhs*=lhs);
 }
-frq_t operator*(frq_t lhs, double const& rhs) {
+frq_t operator*(frq_t lhs, double rhs) {
 	return (lhs*=rhs);
 }
 
-bool operator!=(frq_t const& lhs, frq_t const& rhs) {
-	return !(lhs == rhs);
-}
-bool operator<(frq_t const& lhs, frq_t const& rhs) {
-	return (!(lhs > rhs) && lhs != rhs);
-}
-bool operator>=(frq_t const& lhs, frq_t const& rhs) {
-	return (lhs > rhs || lhs == rhs);
-}
-bool operator<=(frq_t const& lhs, frq_t const& rhs) {
-	return (lhs < rhs || lhs == rhs);
-}
 
 
--- aulib/types/frq_t.h
+++ aulib/types/frq_t.h
@@ -7,39 +7,18 @@ class oct_t;
 //
 // The frq_t class
 //
-// Represents a frequency in units of Hz, kHz, MHz, ...
-// As frequencies < 0 Hz make no physical sense, a frq_t only represents
-// values >= 0.  The constructor interprets the value argument v as 
-// v = fmax(v, 0.0). So, 
+// Represents a frequency in units of Hz.  As frequencies < 0 Hz make no physical sense, a 
+// frq_t only represents values >= 0.  std::abort() is called upon any operation causing the
+// value to fall below 0.  
 //
-// frq_t myfrq {-23.123, frq_t::unit::Hz};
-// is interpreted as: 
-// frq_t myfrq {23.123, frq_t::unit::Hz};
+// frq_t/frq_t, returns a double, not a frq_t.  Thus, the numerical value of a frq_t can be 
+// extracted with operator/(frq_t).  To convert some frq_t object myfrq to, for example, a 
+// "number of Hz:"  
+// auto number_of_Hz = myfrq/frq_t{1};
 //
-// This is also enforced by any opperators which might create a frequency
-// < 0.  For example, if the rhs argument is > the lhs argument,
-// operator-(frq_t,frq_t) returns a frq_t representing 0 Hz (the same 
-// behavior as when the constructor is called with a value < 0).  
-//
-// Internally, the value is stored as a double in "units" of Hz no matter
-// how the object was constructed.  For example, if a frq_t object is 
-// constructed like: 
-// frq_t myfrq {100, frq_t::unit::kHz};
-// the private variable myfrq.m_frq == 0.1, but myfrq.m_unit => kHz.  
-// This is done so frq_t objects can be compared, added, divided, etc without
-// having to invoke mathematical operations involving the unit system.  The
-// value of m_unit is relevant only for print() and for interpreting the
-// value passed to the constructor.  
-//
-// frq_t/frq_t, returns a double, not a frq_t.  Thus, the numerical 
-// value of a frq_t can be extracted with operator/(frq_t).  To convert some 
-// frq_t object myfrq to, for example, a "number of kHz:"  
-// 
-// auto number_of_kHz = myfrq/frq_t{1,frq_t::units::kHz};
-//
-// Undefined operations include:
-// frq_t*frq_t => a (frq_t)^2 which is not a type i have defined
-// 1/frq_t => a (frq_t)^-1 ... as above
+// Explictly deleted operations: 
+// frq_t*frq_t => a (frq_t)^2
+// T/frq_t for all T other than frq_t
 // frq_t +,- double,int, etc
 //
 //
@@ -50,41 +29,56 @@ class oct_t;
 
 class frq_t {
 public:
-	enum class unit : int {
+	/*enum class unit : int {
 		Hz=0,
 		kHz=3,
 		MHz=6,
 		GHz=9
-	};
+	};*/
 
-	explicit frq_t() = default;  // 1 Hz
-	explicit frq_t(double const&, unit const& =frq_t::unit::Hz);
+	frq_t() = default;  // 0 Hz
+	explicit frq_t(double);
+	//explicit frq_t(double, unit const& =frq_t::unit::Hz);
 
-	std::string print(int=3) const;
+	std::string print() const;
 
-	double operator/(frq_t const&) const;
-	frq_t& operator/=(double const&);
-	frq_t& operator*=(double const&);
-	frq_t& operator+=(frq_t const&);
-	frq_t& operator-=(frq_t const&);
-	bool operator==(frq_t const&) const;
-	bool operator>(frq_t const&) const;
+	double operator/(const frq_t&) const;
+	frq_t& operator+=(const frq_t&);
+	frq_t& operator-=(const frq_t&);
+	frq_t& operator/=(double);
+	frq_t& operator*=(double);
 
+	bool operator==(const frq_t&) const;
+	bool operator!=(const frq_t&) const;
+	bool operator<(const frq_t&) const;
+	bool operator>(const frq_t&) const;
+	bool operator<=(const frq_t&) const;
+	bool operator>=(const frq_t&) const;
 private:
-	double m_frq {1.0};  // Units are always Hz
-	int m_unit {0}; // 1=>Hz, 1000=>kHz, 10^6=>MHz, ...
+	double m_frq {0.0};  // Units are always Hz
+	//int m_unit {0};  // 1=>Hz, 1000=>kHz, 10^6=>MHz, ...
 	
-	static const int m_prec;  // precision for operator ==
+	//static const int m_prec;  // precision for operator ==
 };
 
-frq_t operator+(frq_t, frq_t const&);
-frq_t operator-(frq_t, frq_t const&);
-frq_t operator*(double const&, frq_t);
-frq_t operator*(frq_t, double const&);
-bool operator<(frq_t const&, frq_t const&);
-bool operator!=(frq_t const&, frq_t const&);
-bool operator>=(frq_t const&, frq_t const&);
-bool operator<=(frq_t const&, frq_t const&);
+frq_t operator+(frq_t, const frq_t&);
+frq_t operator-(frq_t, const frq_t&);
+frq_t operator*(double, frq_t);
+frq_t operator*(frq_t, double);
+
+// Forbid anything involving 1/frq_t other than frq_t/frq_t, ex, double/frq_t.  
+template <typename Tnum, typename Tresult, typename std::enable_if<!std::is_same<Tnum,frq_t>::value>::type>
+Tresult operator/(Tnum, frq_t)=delete;  
+
+// Forbid anything involving frq_t +/- anything other than another frq_t.  
+template <typename Tlhs, typename Tresult, typename std::enable_if<!std::is_same<Tlhs,frq_t>::value>::type>
+Tresult operator+(frq_t, Tlhs)=delete;  
+template <typename Trhs, typename Tresult, typename std::enable_if<!std::is_same<Trhs,frq_t>::value>::type>
+Tresult operator+(Trhs,frq_t)=delete;  
+template <typename Tlhs, typename Tresult, typename std::enable_if<!std::is_same<Tlhs,frq_t>::value>::type>
+Tresult operator-(frq_t, Tlhs)=delete;  
+template <typename Trhs, typename Tresult, typename std::enable_if<!std::is_same<Trhs,frq_t>::value>::type>
+Tresult operator-(Trhs,frq_t)=delete;  
 
 
 
