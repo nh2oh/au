ddd13f43319478a6635b29843b47d31f9adb6836
ben knowles <ben_junk@ionophore.com>
2019-06-15 09:39:25 -0500

    Fixed conversion from std::vector<>::iterator's to mtrk_iterator's; removed the mtrk_t.from_vec_iterator() family of functions

---
+++ gt_aulib/mtrk_t_split_merge_test.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include "mtrk_test_data.h"
+#include "..\aulib\input\midi\mtrk_t.h"
+#include "..\aulib\input\midi\mtrk_event_t.h"
+#include <vector>
+#include <cstdint>
+#include <iterator>
+
+using namespace mtrk_tests;
+
+mtrk_t make_tsb() {
+	auto mtrk_tsb = mtrk_t();  // auto to avoid MVP
+	for (const auto& e : tsb) {
+		auto curr_ev = mtrk_event_t(e.d.data(),e.d.size());
+		mtrk_tsb.push_back(curr_ev);
+	}
+	return mtrk_tsb;
+}
+
+
+// 
+// OIt split_copy_if(InIt beg, InIt end, OIt dest, UPred pred)
+//
+TEST(mtrk_t_tests, SplitCopyIfForNoteNum67WithTSB) {
+	auto mtrk_b = make_tsb();
+
+	auto isntnum43 = [](const mtrk_event_t& ev)->bool {
+		auto md = get_channel_event(ev);
+		return (is_channel_voice(ev) && (md.p1==67));  // 67 == 0x43u
+	};
+
+	auto new_mtrk = mtrk_t();
+
+	auto it = split_copy_if(mtrk_b.begin(),mtrk_b.end(),
+		std::back_inserter(new_mtrk),isntnum43);
+
+	for (const auto& e : new_mtrk) {
+		EXPECT_TRUE(is_channel_voice(e));
+		auto md = get_channel_event(e);
+		EXPECT_EQ(md.p1,67);
+	}
+
+}
+
+
--- gt_aulib/mtrk_test_data.h
+++ gt_aulib/mtrk_test_data.h
@@ -15,10 +15,20 @@ struct eventdata_with_cumtk_t {
 	std::vector<unsigned char> d {};
 	uint64_t cumtk {};
 };
+struct eventdata_with_cumtk_and_tkonset_t {
+	std::vector<unsigned char> d {};
+	uint64_t cumtk {};
+	uint64_t tkonset {};
+};
 // Test set a
 // Created June 09 2019
 extern std::vector<eventdata_with_cumtk_t> tsa;
 extern mtrk_properties_t tsa_props;
+
+// Test set b
+// Created June 15, 2019
+extern std::vector<eventdata_with_cumtk_and_tkonset_t> tsb;
+
 
 };  // namespace mtrk_tests
 
