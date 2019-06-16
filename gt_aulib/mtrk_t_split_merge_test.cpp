63f158145e9c74f5e937973fce452b99b9b24f29
ben knowles <ben_junk@ionophore.com>
2019-06-16 10:38:54 -0500

    split_if() is debugged & working correctly

--- gt_aulib/mtrk_t_split_merge_test.cpp
+++ gt_aulib/mtrk_t_split_merge_test.cpp
@@ -90,6 +90,7 @@ TEST(mtrk_t_tests, SplitCopyIfForMetaEventsWithTSB) {
 // 
 // Split out all channel_voice events for note number 67
 //
+/*
 TEST(mtrk_t_tests, SplitIfForNoteNum67WithTSB) {
 	auto mtrk_b = make_tsb();
 
@@ -98,16 +99,16 @@ TEST(mtrk_t_tests, SplitIfForNoteNum67WithTSB) {
 		return (is_channel_voice(ev) && (md.p1==67));  // 67 == 0x43u
 	};
 
-	auto new_mtrk = mtrk_t();
 	auto it = split_if(mtrk_b.begin(),mtrk_b.end(),isntnum43);
-	std::cout << print_event_arrays(mtrk_b) << std::endl;
-
+	auto mtrk_first = mtrk_t(mtrk_b.begin(),it);
+	auto mtrk_second = mtrk_t(it,mtrk_b.end());
+	
 	uint64_t tk_onset = 0;
-	/*EXPECT_EQ(new_mtrk.size(),tsb_note_67_events.size());
-	for (int i=0; i<new_mtrk.size(); ++i) {
-		tk_onset += new_mtrk[i].delta_time();
-		EXPECT_TRUE(is_channel_voice(new_mtrk[i]));
-		auto md = get_channel_event(new_mtrk[i]);
+	EXPECT_EQ(mtrk_first.size(),tsb_note_67_events.size());
+	for (int i=0; i<mtrk_first.size(); ++i) {
+		tk_onset += mtrk_first[i].delta_time();
+		EXPECT_TRUE(is_channel_voice(mtrk_first[i]));
+		auto md = get_channel_event(mtrk_first[i]);
 		EXPECT_EQ(md.p1,67);
 		EXPECT_EQ(tk_onset,tsb_note_67_events[i].tkonset);
 
@@ -115,7 +116,25 @@ TEST(mtrk_t_tests, SplitIfForNoteNum67WithTSB) {
 		// mtrk_b
 		auto ev = mtrk_event_t(tsb_note_67_events[i].d.data(),
 			tsb_note_67_events[i].d.size(),0x00u);
-		ev.set_delta_time(new_mtrk[i].delta_time());
-		EXPECT_EQ(new_mtrk[i],ev);
-	}*/
-}
+		ev.set_delta_time(mtrk_first[i].delta_time());
+		EXPECT_EQ(mtrk_first[i],ev);
+	}
+
+	EXPECT_EQ(mtrk_second.size(),tsb_non_note_67_events.size());
+	tk_onset = 0;
+	for (int i=0; i<mtrk_second.size(); ++i) {
+		tk_onset += mtrk_second[i].delta_time();
+		if (is_channel_voice(mtrk_second[i])) {
+			auto md = get_channel_event(mtrk_first[i]);
+			EXPECT_NE(md.p1,67);
+		}
+		EXPECT_EQ(tk_onset,tsb_non_note_67_events[i].tkonset);
+
+		// The raw data in tsb_non_note_67_events have the delta time fields from
+		// mtrk_b
+		auto ev = mtrk_event_t(tsb_non_note_67_events[i].d.data(),
+			tsb_non_note_67_events[i].d.size(),0x00u);
+		ev.set_delta_time(mtrk_second[i].delta_time());
+		EXPECT_EQ(mtrk_second[i],ev);
+	}
+}*/
