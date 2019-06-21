5d2c5b2eaaa1d76fed34100dc313cd3fbc06ee05
ben knowles <ben_junk@ionophore.com>
2019-06-21 10:45:22 -0500

    make_tempo() and new byte manipulation functions in midi_vlq.h are debugged and passing unit tests

--- gt_aulib/mtrk_event_t_meta_factory_funcs_tests.cpp
+++ gt_aulib/mtrk_event_t_meta_factory_funcs_tests.cpp
@@ -16,9 +16,17 @@ TEST(mtrk_event_t_meta_factories, makeTempo) {
 	};
 
 	std::vector<test_t> tests {
-		//{0, 0, 0, 0},
+		{0, 0, 0, 0},
 		{1, 1, 1, 1},
-		{128, 1523, 128, 1523}
+		{128, 1523, 128, 1523},
+		{125428, 1523, 125428, 1523},
+		// 16777215 == 0x00'FF'FF'FF is the largest possible 24-bit int
+		{1,16777215,1,16777215}, 
+		// In the next two, the value for tempo_in exceeds the max value, so
+		// will be truncated to the max value of 16777215.  
+		{1,16777216,1,16777215}, 
+		{1,0xFFFFFFFFu,1,16777215},
+		
 	};
 	for (const auto& e : tests) {
 		auto ev = make_tempo(e.dt_in,e.tempo_in);
