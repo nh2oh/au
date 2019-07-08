0f5c59cb67d256ed594cba92f81deec056f0cb2d
ben knowles <ben_junk@ionophore.com>
2019-07-08 09:36:30 -0500

    MThd ctors; removed the gross set(...) methods

--- gt_aulib/mthd_t_test.cpp
+++ gt_aulib/mthd_t_test.cpp
@@ -4,15 +4,41 @@
 #include <vector>
 #include <cstdint>
 
+TEST(mthd_container_tests, defaultCtor) {
+	std::vector<unsigned char> ans {
+		0x4Du,0x54u,0x68u,0x64u, 0x00u,0x00u,0x00u,0x06u,
+		0x00u,0x01u, 0x00u,0x00u, 0x00u,0x78u
+	};
+	auto mthd = mthd_t();
+	EXPECT_EQ(mthd.size(), ans.size());
+	EXPECT_EQ(mthd.nbytes(), ans.size());
+	
+	auto p = mthd.data();
+	auto it=mthd.begin(); auto cit=mthd.cbegin();
+	auto end=mthd.end(); auto cend=mthd.cend();
+	for (int i=0; i<mthd.size(); ++i) {
+		EXPECT_EQ(*p++,ans[i]);
+		EXPECT_EQ(mthd[i],ans[i]);
+		EXPECT_NE(it,end);
+		EXPECT_NE(cit,cend);
+		EXPECT_EQ(*it++,ans[i]);
+		EXPECT_EQ(*cit++,ans[i]);
+	}
+	EXPECT_EQ(it,end);
+	EXPECT_EQ(cit,cend);
+
+	EXPECT_EQ(mthd.length(),6);
+	time_division_t tdf_ans {0x0078u};
+	EXPECT_EQ(mthd.division().val_,tdf_ans.val_);
+	EXPECT_EQ(mthd.format(),1);
+	EXPECT_EQ(mthd.ntrks(),0);
+}
+
 
 // 
 // From p.133 of the midi std
 //
 TEST(mthd_container_tests, interpretSMPTEField) {
-	/*struct tests_t {
-		uint16_t input {0};
-		midi_smpte_field ans {};
-	};*/
 	struct test_t {
 		uint16_t input {0};
 		int8_t ans_tcf {0};
@@ -33,14 +59,6 @@ TEST(mthd_container_tests, interpretSMPTEField) {
 		EXPECT_EQ(curr_tcf,e.ans_tcf);
 		auto curr_upf = get_units_per_frame(curr_tdf);
 		EXPECT_EQ(curr_upf,e.ans_upf);
-		/*
-		auto curr_ft = detect_midi_time_division_type(e.input);
-		EXPECT_EQ(curr_ft,midi_time_division_field_type_t::SMPTE);
-
-		auto curr = interpret_smpte_field(e.input);
-		EXPECT_EQ(curr.time_code_fmt,e.ans.time_code_fmt);
-		EXPECT_EQ(curr.units_per_frame,e.ans.units_per_frame);
-		*/
 	}
 
 }
