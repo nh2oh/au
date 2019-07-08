a7de2110a2196867b96203a16ac977ccd7d7b184
ben knowles <ben_junk@ionophore.com>
2019-07-08 08:49:30 -0500

    functions for working with the time-division-field within an MThd

--- gt_aulib/mthd_t_test.cpp
+++ gt_aulib/mthd_t_test.cpp
@@ -9,24 +9,38 @@
 // From p.133 of the midi std
 //
 TEST(mthd_container_tests, interpretSMPTEField) {
-	struct tests_t {
+	/*struct tests_t {
 		uint16_t input {0};
 		midi_smpte_field ans {};
+	};*/
+	struct test_t {
+		uint16_t input {0};
+		int8_t ans_tcf {0};
+		uint8_t ans_upf {0};
 	};
-
-	std::vector<tests_t> tests {
-		{0xE250u,{-30,80}},  // p.133 of the midi std
-		{0xE728u,{-25,40}},   // 25fr/sec * 40tk/fr => 1000tk/sec => ms resolution
-		{0xE350u,{-29,80}}
+	std::vector<test_t> tests {
+		{0xE250u,-30,80},  // p.133 of the midi std
+		{0xE728u,-25,40},   // 25fr/sec * 40tk/fr => 1000tk/sec => ms resolution
+		{0xE350u,-29,80}
 	};
 
 	for (const auto& e : tests) {
+		time_division_t curr_tdf;
+		curr_tdf.val_ = e.input;
+		EXPECT_EQ(type(curr_tdf),time_division_t::type::smpte);
+
+		auto curr_tcf = get_time_code_fmt(curr_tdf);
+		EXPECT_EQ(curr_tcf,e.ans_tcf);
+		auto curr_upf = get_units_per_frame(curr_tdf);
+		EXPECT_EQ(curr_upf,e.ans_upf);
+		/*
 		auto curr_ft = detect_midi_time_division_type(e.input);
 		EXPECT_EQ(curr_ft,midi_time_division_field_type_t::SMPTE);
 
 		auto curr = interpret_smpte_field(e.input);
 		EXPECT_EQ(curr.time_code_fmt,e.ans.time_code_fmt);
 		EXPECT_EQ(curr.units_per_frame,e.ans.units_per_frame);
+		*/
 	}
 
 }
