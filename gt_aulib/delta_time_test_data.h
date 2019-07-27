ae827a5648a28d866b38271c417f53c202607be7
ben knowles <ben_junk@ionophore.com>
2019-07-27 11:03:56 -0500

    New delta-time unit-test data

--- gt_aulib/delta_time_test_data.h
+++ gt_aulib/delta_time_test_data.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <array>
 #include <cstdint>
 
 // Delta-time test data
@@ -14,4 +15,18 @@ extern const std::vector<dt_test_data_t> dt_test_set_a;
 extern const std::vector<dt_test_data_t> dt_test_set_b;
 extern const std::vector<dt_test_data_t> dt_test_set_c;
 // extern const std::vector<dt_test_data_t> dt_test_set_d;
+
+
+// Manually encoded fields
+// Set B:  Values on [0,max-allowed-dt]; fields are _not_ encoded in the
+// most compact way possible.  
+struct dt_fields_t {
+	std::array<unsigned char,8> ans {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+	int32_t ans_val {0};
+	int ans_n_bytes {0};
+};
+extern const std::vector<dt_fields_t> dt_fields_test_set_a;
+extern const std::vector<dt_fields_t> dt_fields_test_set_b;
+
+
 
