7cd3b7bcd9057659c549a99e3359800a1d8e66ed
ben knowles <ben_junk@ionophore.com>
2019-08-13 09:06:55 -0500

    Improve accuracy for ticks2sec() and sec2ticks(); unit test

--- gt_aulib/midi_tempo_and_timediv_tests.cpp
+++ gt_aulib/midi_tempo_and_timediv_tests.cpp
@@ -1,17 +1,15 @@
 #include "gtest/gtest.h"
 #include "..\aulib\input\midi\midi_raw.h"
-#include "..\aulib\input\midi\smf_t.h"
 #include <cstdint>
-#include <array>
-#include <filesystem>
 #include <cmath>
 #include <vector>
+#include <limits>
+
 
 // 
-// Tests of:
-// ...
+// Tests of ticks2sec() and sec2ticks()
 // 
-TEST(midi_tempo_and_timediv_tests, interconversionSecondsTicks) {
+TEST(midi_tempo_and_timediv_tests, maxErrorTickSecondInterconversion) {
 	std::vector<time_division_t> tdivs {
 		// Small values
 		time_division_t(1),time_division_t(2),time_division_t(3),
@@ -21,31 +19,35 @@ TEST(midi_tempo_and_timediv_tests, interconversionSecondsTicks) {
 		// Max allowed value for ticks-per-qnt
 		time_division_t(0x7FFF)
 	};
-	std::vector<int32_t> tempos {1,2,3,250000,500000,750000,0xFFFFFF};//,0};
-	std::vector<int32_t> tks {1,2,3,154,4287,24861,251111,52467,754714,
-		0x7FFFFFFF,0};
-	
-	double max_permissible_err_s = 1.0/1000000000.0;  // 1 ns
-	double cum_err = 0.0;
+	std::vector<int32_t> tempos {0,1,2,3,250000,500000,750000,0xFFFFFF};
+	std::vector<int32_t> tks {0,1,2,3,154,4287,24861,251111,52467,754714,
+		0x7FFFFFFF};
+
 	for (const auto& tdiv: tdivs) {
 		for (const auto& tempo: tempos) {
 			for (const auto& tk : tks) {
-				double curr_tpq = static_cast<double>(tdiv.get_tpq());  // tks/q-nt
-				double curr_uspq = static_cast<double>(tempo);  // us/q-nt
-				double curr_spq = curr_uspq/1000000.0;  // us/q-nt
-				double curr_sptk = curr_spq/curr_tpq;  // "seconds-per-tick"
+				auto s_from_tk = ticks2sec(tk,tdiv,tempo);
+				if (tempo==0) {
+					// If the tempo is 0, sec obviously == 0 no matter the
+					// value of tk, however, you can't go back to tk...
+					continue;
+				}
+				// ticks2sec() returns a value within 1 ulp of the exact value
+				auto max_err_computed_sec = 
+					std::numeric_limits<double>::epsilon()*s_from_tk;
+				auto s_from_tk_min = s_from_tk - max_err_computed_sec;
+				auto s_from_tk_max = s_from_tk + max_err_computed_sec;
 
-				auto expect_s = (tk*curr_spq)/curr_tpq;
-				
-				auto s = ticks2sec(tk,tdiv,tempo);
-				auto err = std::abs(s-expect_s);
-				
-				EXPECT_LT(err,max_permissible_err_s);
+				auto tk_from_s = sec2ticks(s_from_tk,tdiv,tempo);
+				auto tk_from_s_min = sec2ticks(s_from_tk_min,tdiv,tempo);
+				auto tk_from_s_max = sec2ticks(s_from_tk_max,tdiv,tempo);
+
+				double curr_tk = static_cast<double>(tk);
+				EXPECT_GE(curr_tk,tk_from_s_min);
+				EXPECT_LE(curr_tk,tk_from_s_max);
 			}
 		}
 	}
-	//auto cum_sec = ticks2sec(cum_num_tks,tc.tdiv,tc.tempo);
-	//auto cum_tk = sec2ticks(cum_sec,tc.tdiv,tc.tempo);
 }
 
 
